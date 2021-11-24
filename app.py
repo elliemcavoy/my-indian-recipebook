1821941e92e80d1ad8be1789c609c70c6bf145a9
elliemcavoy <ellie.mcavoy@outlook.com>
2021-11-24 22:57:25 +0000

    Credentials added to env.py file

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ from flask import (
 
 
 app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
