5c680b5c7a973dbad0bc3906643bf65cc41118bd
elliemcavoy <ellie.mcavoy@outlook.com>
2021-11-25 22:14:27 +0000

    Add index function to test app is working

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+mongo = PyMongo(app)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
