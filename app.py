1206f4a80fdd59fa75de111a1a867b480f1c98a3
elliemcavoy <ellie.mcavoy@outlook.com>
2021-11-25 19:28:46 +0000

    Procfile and requirements.txt created

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
-
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
@@ -10,3 +11,10 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
+
